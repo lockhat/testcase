eb580e04f28debca65aed1149ed3ba3f681d769b
lockhat <1160444584@qq.com>
2017-12-18 16:50:19 +0800

    test00

--- test_case/test_send_emails.py
+++ test_case/test_send_emails.py
@@ -29,7 +29,7 @@ class Test(unittest.TestCase):
         #收件人
         driver.find_element_by_css_selector(".nui-editableAddr-ipt").send_keys("15156214261@163.com")
 
-
+        #发送还是有问题
         driver.find_element_by_xpath("/html/body/div[2]/div/div[2]/header/div/div/div/span[2]").click()
         driver.find_element_by_xpath("//*[@class='nui-msgbox-ft-btns']/div/span").click()
         text = driver.find_element_by_class_name('tK1').text
